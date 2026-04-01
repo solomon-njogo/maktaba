d79b7271d6dff21cfa2ca1cfa589baaf6e7a3b16
Solomon Njogo <85402865+solomon-njogo@users.noreply.github.com>
2026-04-01 13:00:22 +0300

    fix ahref error

--- maktaba-app/app/(tabs)/_layout.tsx
+++ maktaba-app/app/(tabs)/_layout.tsx
@@ -62,7 +62,6 @@ export default function TabLayout() {
         name="add-placeholder"
         options={{
           title: '',
-          href: null,
           tabBarButton: () => <CenterAddButton />,
         }}
       />
